2774ea8dd6a2c68892bfc14514cfce37bcb663a5
Samantha Vale <karlasamantha@gmail.com>
2021-03-20 19:30:10 -0300

    initial changes

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -1,16 +1,8 @@
-import * as Style from './styles'
-
 const Main = () => (
-  <Style.Wrapper>
-    <Style.Title>Boilerplate</Style.Title>
-    <Style.Description>
-      NextJS, Typescript, GraphQL e Styled Components
-    </Style.Description>
-    <Style.Illustration
-      src="/img/illustration.svg"
-      alt="Pessoa desenvolvedora de frente a tela com código"
-    />
-  </Style.Wrapper>
+  <div>
+    <h1>NextJS</h1>
+    <a href="#">teste</a>
+  </div>
 )
 
 export default Main
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,14 +7,14 @@ function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>NextJS na prática</title>
+        <title>Trips</title>
         <link rel="shortcut icon" href="/img/icon-512.png" />
         <link rel="apple-touch-icon" href="/img/icon-512.png" />
         <link rel="manifest" href="/manifest.json" />
         <meta name="theme-color" content="#06092b" />
         <meta
           name="description"
-          content="A simple project starter to work with TypeScript, React, NextJS and Styled Components"
+          content="A simple project to show my favorite places in the world!"
         />
       </Head>
       <GlobalStyles />
