ffb0de826a52d906a83f426c23d364c00a6b9bf7
Samantha Vale <karlasamantha@gmail.com>
2021-03-20 19:04:53 -0300

    Initial commit from Create Next App

---
+++ src/components/Main/index.jsx
@@ -0,0 +1,16 @@
+import * as Style from './styles'
+
+const Main = () => (
+  <Style.Wrapper>
+    <Style.Title>Boilerplate</Style.Title>
+    <Style.Description>
+      NextJS, Typescript, GraphQL e Styled Components
+    </Style.Description>
+    <Style.Illustration
+      src="/img/illustration.svg"
+      alt="Pessoa desenvolvedora de frente a tela com código"
+    />
+  </Style.Wrapper>
+)
+
+export default Main
