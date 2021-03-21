9e442d885cbc3bc132b95d7101797a23a297bc40
Samantha Vale <karlasamantha@gmail.com>
2021-03-21 17:33:03 -0300

    adds icon to place page

--- src/templates/Places/index.tsx
+++ src/templates/Places/index.tsx
@@ -1,3 +1,8 @@
+import LinkWrapper from 'components/LinkWrapper'
+import { CloseOutline } from '@styled-icons/evaicons-outline'
+
+import * as Style from './styles'
+
 export type ImageProps = {
   url: string
   height: number
@@ -18,13 +23,25 @@ export type PlacesTemplateProps = {
 export default function PlacesTemplate({ place }: PlacesTemplateProps) {
   return (
     <>
-      <h1>{place.name}</h1>
+      <LinkWrapper href="/">
+        <CloseOutline size={32} aria-label="Go back to map" />
+      </LinkWrapper>
 
-      <div dangerouslySetInnerHTML={{ __html: place.description?.html }} />
+      <Style.Wrapper>
+        <Style.Container>
+          <Style.Heading>{place.name}</Style.Heading>
 
-      {place.gallery.map((image, index) => (
-        <img key={`photo-${index}`} src={image.url} alt={place.name} />
-      ))}
+          <Style.Body
+            dangerouslySetInnerHTML={{ __html: place.description?.html }}
+          />
+
+          <Style.Gallery>
+            {place.gallery.map((image, index) => (
+              <img key={`photo-${index}`} src={image.url} alt={place.name} />
+            ))}
+          </Style.Gallery>
+        </Style.Container>
+      </Style.Wrapper>
     </>
   )
 }
